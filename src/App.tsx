734fc88df3915514e1206bffe277b77a84e3024e
Giacomo Rossi <giacomorossi.dev@gmail.com>
2025-12-23 22:31:06 +0100

    feat: app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import doveConvieneLogo from "/dove-conviene.png";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { ToggleGroup, ToggleGroupItem } from "./components/ui/toggle-group";
+import { BookmarkIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [city, setCity] = useState("Rimini");
+  const [categories, setCategories] = useState(["cura-casa-e-corpo"]);
+  const [flyers, setFlyers] = useState(["acqua & sapone"]);
+  const [products, setProducts] = useState("pizza,chilly");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <header>
+        <div className="flex gap-4 items-center justify-between h-20 shadow">
+          <div className="container ">
+            <div className="max-w-50">
+              <img src={doveConvieneLogo} alt="logo" />
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="py-10">
+        <div className="container">
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label>Località</Label>
+              <Input
+                onInput={(e) => setCity(e.currentTarget.value)}
+                value={city}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Categorie</Label>
+              <ToggleGroup
+                value={categories}
+                onValueChange={(categories) => setCategories(categories)}
+                type="multiple"
+                variant="outline"
+                spacing={2}
+                size="sm"
+              >
+                <ToggleGroupItem
+                  value=" iper-e-super"
+                  aria-label="Toggle star"
+                  className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-red-500 data-[state=on]:*:[svg]:stroke-red-500"
+                >
+                  <BookmarkIcon />
+                  iper-e-super
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="cura-casa-e-corpo"
+                  aria-label="Toggle heart"
+                  className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-red-500 data-[state=on]:*:[svg]:stroke-red-500"
+                >
+                  <BookmarkIcon />
+                  cura-casa-e-corpo
+                </ToggleGroupItem>
+              </ToggleGroup>
+            </div>
 
-export default App
+            <div className="space-y-2">
+              <Label>Volantini</Label>
+              <ToggleGroup
+                value={flyers}
+                onValueChange={(flyers) => setFlyers(flyers)}
+                type="multiple"
+                variant="outline"
+                spacing={2}
+                size="sm"
+              >
+                <ToggleGroupItem
+                  value="conad superstore"
+                  aria-label="Toggle star"
+                  className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-red-500 data-[state=on]:*:[svg]:stroke-red-500"
+                >
+                  <BookmarkIcon />
+                  conad superstore
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="acqua & sapone"
+                  aria-label="Toggle heart"
+                  className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-red-500 data-[state=on]:*:[svg]:stroke-red-500"
+                >
+                  <BookmarkIcon />
+                  acqua & sapone
+                </ToggleGroupItem>
+              </ToggleGroup>
+            </div>
+            <div className="space-y-2">
+              <Label>Prodotti</Label>
+              <Input
+                onInput={(e) => setProducts(e.currentTarget.value)}
+                value={products}
+              />
+            </div>
+            <Alert>
+              <AlertTitle>Query</AlertTitle>
+              <AlertDescription>
+                <div>Città: {city}</div>
+                <div>Categorie: {categories.join(", ")}</div>
+                <div>Volantini: {flyers.join(", ")}</div>
+                <div>Prodotti: {products.split(",").join(", ")}</div>
+              </AlertDescription>
+            </Alert>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
